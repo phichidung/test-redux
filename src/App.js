28f18eb3ee5d47858f4b0c83b6d3969f429d12c4
phichidung <benevolentwarriors@gmail.com>
2019-10-21 07:46:29 +0700

    moved on { Provider } and store into index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import Main from './Main';
-import { Provider } from 'react-redux';
-import store from './store';
 
 export default class App extends Component {
 
     render() {
         return (
-            <Provider store={store}>
-                <Main/>
-            </Provider>
+            <Main/>
         );
     }
 }
